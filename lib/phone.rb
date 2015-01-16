e419bb9fcff8455788e2e9fdb5ab73504f2d6458
Monty Rohde <nathanarohde@gmail.com>
2015-01-16 14:28:01 -0800

    Changed phone.rb to receive three seperate phone numbers rather than receiving a single concated string from app.rb, specs for contact_spec not yet changed.

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,9 +1,14 @@
 class Phone
   @@allphonenumbers = []
-  attr_reader(:phone)
+  attr_reader(:home_phone, :work_phone, :cell_phone)
+  @home_phone = ""
+  @work_phone = ""
+  @cell_phone = ""
 
   define_method(:initialize) do |attributes|
-    @phone = attributes.fetch(:phone)
+    @home_phone = attributes.fetch(:home_phone)
+    @work_phone = attributes.fetch(:work_phone)
+    @cell_phone = attributes.fetch(:cell_phone)
   end
 
   define_singleton_method(:all) do
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -3,8 +3,24 @@ require ('rspec')
 
 describe('#phone') do
   it('Inputs a home phone number') do
-    phone_test = Phone.new({:phone => "1111111111"})
+    phone_test = Phone.new({:home_phone => "1111111111", :work_phone => "", :cell_phone => ""})
     phone_test.save()
-    expect(phone_test.phone()).to(eq("1111111111"))
+    expect(phone_test.home_phone()).to(eq("1111111111"))
+  end
+end
+
+describe('#phone') do
+  it('Inputs a work phone number') do
+    phone_test = Phone.new({:home_phone => "1111111111", :work_phone => "2222222222", :cell_phone => ""})
+    phone_test.save()
+    expect(phone_test.work_phone()).to(eq("2222222222"))
+  end
+end
+
+describe('#phone') do
+  it('Inputs a cell phone number') do
+    phone_test = Phone.new({:home_phone => "1111111111", :work_phone => "2222222222", :cell_phone => "3333333333"})
+    phone_test.save()
+    expect(phone_test.cell_phone()).to(eq("3333333333"))
   end
 end
