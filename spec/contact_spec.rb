a603f8b174910303c2c0a413a384448b536862a7
Monty Rohde <nathanarohde@gmail.com>
2015-01-16 10:50:42 -0800

    Class Contact successfully takes a phone number.

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -12,3 +12,15 @@ describe('#name') do
     expect(contact_test.name().name()).to(eq("Dude"))
   end
 end
+
+describe('#phone') do
+  it ('Checks to see if class contact returns a phone number') do
+    name_test = Name.new({:name => "Dude"})
+    name_test.save()
+    phone_test = Phone.new({:phone => "1111111111"})
+    phone_test.save()
+    contact_test = Contact.new({:name => name_test, :phone => phone_test})
+    contact_test.save()
+    expect(contact_test.phone().phone()).to(eq("1111111111"))
+  end
+end
