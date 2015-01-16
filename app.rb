a5c5442197bfa97e6cdd5673bc6a2872256d6775
Monty Rohde <nathanarohde@gmail.com>
2015-01-16 14:10:38 -0800

    Class phone successfully takes and outputs all three inputs however the work is done in one very ugly complicated line. app.rb ln18  Attempt to improve by better using hashes in later edits.

--- app.rb
+++ app.rb
@@ -15,7 +15,9 @@ end
 post ('/submit') do
   new_name=Name.new({:name => params.fetch('name')})
   new_name.save()
-  new_phone=Phone.new({:phone => params.fetch('home_phone')})
+  new_phone=Phone.new({:phone => ((((((("Home phone: ").concat((params.fetch('home_phone')))).concat(" Work phone: ")).concat((params.fetch('work_phone')))).concat(" Cell phone: "))).concat((params.fetch('cell_phone'))))  })
+    # ("Home phone: " + (params.fetch('home_phone')) + " Work phone: " + (params.fetch('work_phone')) + " Cell phone: "(params.fetch('cell_phone'))).concat() })
+    # ((params.fetch('home_phone').concat((params.fetch('work_phone').concat((params.fetch('cell_phone'))))))) })
   new_phone.save()
   new_contact=Contact.new({:name => new_name, :phone => new_phone})
   new_contact.save()
