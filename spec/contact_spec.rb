92807b84bdaecbfd939c00208fbd628f47ba0fe5
Monty Rohde <nathanarohde@gmail.com>
2015-01-16 10:56:19 -0800

    Class contact successfully accepts multiple phone numbers.

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -24,3 +24,15 @@ describe('#phone') do
     expect(contact_test.phone().phone()).to(eq("1111111111"))
   end
 end
+
+describe('#phone') do
+  it ('Checks to see if class contact returns multiple phone numbers') do
+    name_test = Name.new ({:name => "Dude"})
+    name_test.save()
+    phone_test = Phone.new({:phone => "1111111111, 2222222222"})
+    phone_test.save()
+    contact_test = Contact.new({:name => name_test, :phone => phone_test})
+    contact_test.save()
+    expect(contact_test.phone().phone()).to(eq("1111111111, 2222222222"))
+  end
+end
